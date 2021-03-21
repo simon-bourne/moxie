249f6a7bc482403ec77efe7130a1fcd6e379468e
Simon Bourne <simonbourne@gmail.com>
2021-03-21 13:56:59 +0000

    Support children

--- dom/examples/custom_html_element/src/lib.rs
+++ dom/examples/custom_html_element/src/lib.rs
@@ -1,12 +1,22 @@
 // TODO: `topo` hygeine
-use moxie_dom::html_element;
-use wasm_bindgen::prelude::*;
+use moxie_dom::{html_element, interfaces::node::Parent};
+use wasm_bindgen::prelude::wasm_bindgen;
 
 html_element!(
     <my_custom_element>
+
+    categories {
+        Flow, Palpable
+    }
+
+    children {
+        categories {
+            Flow
+        }
+    }
 );
 
 #[wasm_bindgen]
 pub fn boot(root: moxie_dom::raw::sys::Node) {
-    moxie_dom::boot(root, my_custom_element);
+    moxie_dom::boot(root, || my_custom_element().child("Hello, world!!!"));
 }
