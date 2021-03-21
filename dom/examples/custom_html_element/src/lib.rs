5d3538679737273e61e7a7ef74ce9910eb638da8
Simon Bourne <simonbourne@gmail.com>
2021-03-21 18:57:35 +0000

    Try out child tags

--- dom/examples/custom_html_element/src/lib.rs
+++ dom/examples/custom_html_element/src/lib.rs
@@ -1,5 +1,5 @@
 // TODO: `topo` hygeine
-use moxie_dom::{html_element, interfaces::node::Parent};
+use moxie_dom::{elements::html::div, html_element, interfaces::node::Parent};
 use wasm_bindgen::prelude::wasm_bindgen;
 
 html_element!(
@@ -22,10 +22,31 @@ html_element!(
     }
 );
 
+html_element!(
+    <my_custom_div_wrapper["my-custom-div-wrapper"]>
+
+    categories {
+        Flow, Palpable
+    }
+
+    children {
+        tags {
+            <div>
+        }
+    }
+);
+
 #[wasm_bindgen]
 pub fn boot(root: moxie_dom::raw::sys::Node) {
     moxie_dom::boot(root, || {
-        my_custom_element().bool_attr(true)
-            .u32_attr(1234).stringly_typed_attr("abc").child("Hello, world!!!")
+        div()
+            .child(
+                my_custom_element()
+                    .bool_attr(true)
+                    .u32_attr(1234)
+                    .stringly_typed_attr("abc")
+                    .child("Hello, world!!!"),
+            )
+            .child(my_custom_div_wrapper().child(div()))
     });
 }
